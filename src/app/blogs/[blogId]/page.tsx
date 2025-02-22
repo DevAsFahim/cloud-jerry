ed713ca6096ff6920dee34d9855e6521fe19fa4a
DevAsFahim <108362901+DevAsFahim@users.noreply.github.com>
2025-02-22 12:55:26 +0600

    Update

--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -1,14 +1,29 @@
 import BlogDetailsCard from "@/components/ui/BlogDetailsCard";
+import { Blog } from "@/types";
 
+// below code will cash first 3 blog in build time
 export const generateStaticParams = async () => {
-    return [
-        {
-            blogId: '1',
-        },
-        {
-            blogId: '2',
-        },
-    ]
+  const res = await fetch("http://localhost:5000/blogs");
+  const blogs = await res.json();
+
+  return blogs.slice(0, 3).map((blog: Blog) => ({
+    blogId: blog.id,
+  }));
+};
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ blogId: string }>;
+}) {
+  const { blogId } = await params;
+
+  const res = await fetch(`http://localhost:5000/blogs/${blogId}`);
+  const blog = await res.json();
+
+  return {
+    title: blog.title,
+  };
 }
 
 const BlogDetailsPage = async ({
